e5422eda5831ff778ad780d35126b35e9d05cbb1
alex <alexliu12@gmail.com>
2018-12-10 20:41:53 -0800

    made some progress on challenge_2

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,10 @@
-
+// let jsonData = document.getElementById("input").value;
+let formSubmit = (e) => {
+  let jsonData = document.getElementById("input").value;
+  // console.log('data', jsonData) 
+  let jsonStr = JSON.parse(jsonData)
+  console.log(jsonStr);
+  e.preventDefault();
+  console.log(e);
+}
 
