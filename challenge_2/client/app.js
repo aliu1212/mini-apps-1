a31f25693380a1549254d6ee1abdbc11d19d36c5
alex <alexliu12@gmail.com>
2018-12-11 20:09:20 -0800

    made some progress on app #2

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,10 +1,51 @@
 // let jsonData = document.getElementById("input").value;
 let formSubmit = (e) => {
-  let jsonData = document.getElementById("input").value;
-  // console.log('data', jsonData) 
-  let jsonStr = JSON.parse(jsonData)
-  console.log(jsonStr);
   e.preventDefault();
-  console.log(e);
+  // before 1st refactor
+  // let jsonStr = document.getElementById("input").value;
+  // let jsonStr = document.getElementById("filey");
+  let jsonStr = document.querySelector("filey");
+  // console.log(jsonStr,  e)
+  let jsonData = JSON.parse(jsonStr);
+  let people = [];
+  let addme = (obj) => {
+    people.push(obj);
+    if (obj.children) {
+      for (let i = 0; i < obj.children.length; i++) {
+        addme(obj.children[i]);
+      }
+    }
+  }
+  addme(jsonData);
+  let keys = [];
+  let resultStr = '';
+  for (key in people[0]) {
+    if (key !== 'children')
+    keys.push(key);
+  }
+  for (let j = 0; j < keys.length; j++) {
+    resultStr += keys[j];
+    if (j !== keys.length-1) {
+      resultStr += ',';
+    } else {
+      resultStr += '\n';
+    }
+  }
+  for (let k = 0; k < people.length; k++) {
+    for (let l = 0; l < keys.length; l++) {
+      resultStr += people[k][keys[l]];
+      if (l !== keys.length-1) {
+        resultStr += ',';
+      } else {
+        resultStr += '\n';
+      }
+    }
+  }
+  console.log(resultStr);
+  document.getElementById("output").innerHTML = resultStr;
+  // console.log('keys', keys);
+
+
+  // console.log(e);
 }
 
