250802e109eddd44e707a96aa7643f8463cae71c
alex <alexliu12@gmail.com>
2019-03-12 12:44:39 -0700

    progress commit

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,15 +1,18 @@
 const express = require('express');
+const parser = require('body-parser');
 const app = express();
 const path = require('path');
 
 const port = 3000;
 
+app.use(parser.json());
+app.use(parser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, './client')))
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/client/index.html'))
 })
 app.post('/upload_json', (req, res) => {
-  console.log(req.body);
+  console.log('BODY', req.body);
   res.end();
 })
 
